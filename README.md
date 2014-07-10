8dc8bdc3382bb7083107f2e73e8659f0c9f54289
Tom MacWright <tom@macwright.org>
2014-07-10 17:09:59 -0400

    First

---
+++ README.md
@@ -0,0 +1,13 @@
+# chartpipe
+
+Does what it says on the tin.
+
+## install
+
+    npm install -g chartpipe
+
+## usage
+
+```sh
+$ commandthatoutputscsv | chartpipe
+```
---
+++ index.js
@@ -0,0 +1,31 @@
+var concat = require('concat-stream'),
+    fs = require('fs'),
+    GitHubApi = require('github'),
+    github = new GitHubApi({
+        version: '3.0.0',
+        protocol: 'https'
+    }),
+    open = require('open');
+
+console.log('Waiting for input...');
+
+process.stdin.pipe(concat(function(data) {
+    github.gists.create({
+        description: '/dev/chartpipe',
+        public: true,
+        files: {
+            'data.csv': {
+                content: data.toString()
+            },
+            'index.html': {
+                content: fs.readFileSync(__dirname + '/templates/groupedbars.html', 'utf8')
+            }
+        }
+    }, function (err, res) {
+        if (err) {
+            console.error('Unable to create Gist:' + JSON.stringify(res));
+        } else {
+            open('https://bl.ocks.org/d/' + res.id);
+        }
+    });
+}));
