8b40f510b1e90ac9db6a9c6b2a4d923d55648f9d
Drew Bollinger <drew@developmentseed.org>
2014-07-17 23:15:51 -0400

    Added stackedbars

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ if (argv.help) {
     console.log('usage: chartpipe --type=groupedbars file.csv');
     console.log('usage: chartpipe --type=histogram file.csv');
     console.log('usage: chartpipe --type=linechart file.csv')
+    console.log('usage: chartpipe --type=stackedbars file.csv')
     console.log('available charts: ' + typenames);
     return;
 }
